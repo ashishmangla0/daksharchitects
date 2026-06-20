f6f7c26b76ccf3748a25caf78350532190ea89f7
ashish <ashishmangla1991@gmail.com>
2026-06-20 11:01:30 +0530

    inital commit

---
+++ src/app/components/footer/footer.tsx
@@ -0,0 +1,12 @@
+import styles from "./footer.module.scss";
+const Footer = () =>{
+return (
+    <footer className={styles.footer}>
+        <div className={"container"}>
+            I am footer
+        </div>
+    </footer>
+)
+}
+
+export default Footer;
